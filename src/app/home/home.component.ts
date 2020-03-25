c3b188a69b1b70320bbe0d742408fabee9ad6182
Alberto Gonzalez <alberto.gonzalez@avaco.com.ar>
2020-03-24 22:57:28 -0300

    bugfixing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,10 @@ import { ContentSite } from '@app/shared/models/contentsite.model';
 import { Events } from '@app/shared/Models/Events.model';
 import { ExternalProduct } from '@app/shared/Models/ExternalProduct.model';
 
+declare function recortarTituloPrincipal(text);
+declare function recortarSummary(text);
+declare function recortarTituloSecundario(text);
+
 @Component(
     {
         templateUrl: 'home.component.html' ,
@@ -28,6 +32,8 @@ export class HomeComponent {
     }
 
     ngOnInit() {
+
+
         const sectionName = "NOTICIAS";
         const cantMax = 4;
 
@@ -37,11 +43,11 @@ export class HomeComponent {
             map(ret => ret as ContentSite),
             map(ret => {
                 if (ret.items != undefined){
-                    ret.items[0] != undefined && ret.items[0].title != undefined ? ret.items[0].title = $.fn.recortarTituloPrincipal(ret.items[0].title) : false;
-                    ret.items[0] != undefined && ret.items[0].summary != undefined ? ret.items[0].summary = $.fn.recortarSummary(ret.items[0].summary) : false;
-                    ret.items[1] != undefined && ret.items[1].title != undefined ? ret.items[1].title = $.fn.recortarTituloSecundario(ret.items[1].title) : false;
-                    ret.items[2] != undefined && ret.items[2].title != undefined ? ret.items[2].title = $.fn.recortarTituloSecundario(ret.items[2].title): false;
-                    ret.items[3] != undefined && ret.items[3].title != undefined ? ret.items[3].title = $.fn.recortarTituloSecundario(ret.items[3].title): false;
+                    ret.items[0] != undefined && ret.items[0].title != undefined ? ret.items[0].title = recortarTituloPrincipal(ret.items[0].title) : false;
+                    ret.items[0] != undefined && ret.items[0].summary != undefined ? ret.items[0].summary = recortarSummary(ret.items[0].summary) : false;
+                    ret.items[1] != undefined && ret.items[1].title != undefined ? ret.items[1].title = recortarTituloSecundario(ret.items[1].title) : false;
+                    ret.items[2] != undefined && ret.items[2].title != undefined ? ret.items[2].title = recortarTituloSecundario(ret.items[2].title): false;
+                    ret.items[3] != undefined && ret.items[3].title != undefined ? ret.items[3].title = recortarTituloSecundario(ret.items[3].title): false;
                 }
 
                 return ret;
@@ -63,42 +69,7 @@ export class HomeComponent {
             this.externalProduct = data;
         });
 
-        $.fn.textWidth = function(text, font) {
-            if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
-            $.fn.textWidth.fakeEl.text(text || this.val() || this.text()).css('font', font || this.css('font'));
-            return $.fn.textWidth.fakeEl.width();
-        };
         
-        $.fn.recortar = function(text, font, lineWidth) {
-            var size = $.fn.textWidth(text, font);
-            var retext = text;
-            var recortar = size > lineWidth;
-            while (size > lineWidth) {
-                text = text.toString().substr(0,text.length-1);
-                size = $.fn.textWidth(text, font);
-            }
-            
-            if (recortar) {
-                text = text.toString().substr(0,text.length-5);
-                text = text + '...'
-            }
-            
-            return text;
-            
-        };
-
-        $.fn.recortarTituloPrincipal = function(text) {
-            return $.fn.recortar(text, '23pt Roboto', 990);
-        }
-
-        $.fn.recortarTituloSecundario = function(text) {
-            return $.fn.recortar(text, '15pt Roboto', 890);
-        }
-
-        $.fn.recortarSummary = function(text) {
-            return $.fn.recortar(text, '12pt Roboto', 1800);
-        }
-
     }
 
 }
---
+++ src/stylesCustom/js/recortar.js
@@ -0,0 +1,37 @@
+
+
+$.fn.textWidth = function(text, font) {
+    if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+    $.fn.textWidth.fakeEl.text(text || this.val() || this.text()).css('font', font || this.css('font'));
+    return $.fn.textWidth.fakeEl.width();
+};
+
+$.fn.recortar = function(text, font, lineWidth) {
+    var size = $.fn.textWidth(text, font);
+    var retext = text;
+    var recortar = size > lineWidth;
+    while (size > lineWidth) {
+        text = text.toString().substr(0,text.length-1);
+        size = $.fn.textWidth(text, font);
+    }
+    
+    if (recortar) {
+        text = text.toString().substr(0,text.length-5);
+        text = text + '...'
+    }
+    
+    return text;
+    
+};
+
+function recortarTituloPrincipal(text) {
+    return $.fn.recortar(text, '23pt Roboto', 990);
+}
+
+function recortarTituloSecundario(text) {
+    return $.fn.recortar(text, '15pt Roboto', 890);
+}
+
+function recortarSummary(text) {
+    return $.fn.recortar(text, '12pt Roboto', 1800);
+}
