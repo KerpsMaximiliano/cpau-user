887d3440fe92c9d65a8cd62f4806c3a23458da78
Harold Reyes <haroldreyes@scien.tech>
2020-04-26 01:11:12 -0300

    login unified

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,72 +1,87 @@
-﻿import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+﻿import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
-import { environment } from '@environments/environment';
-import { User } from '@app/_models';
+import { environment } from "@environments/environment";
+import { User } from "@app/_models";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class AuthenticationService {
-    private currentUserSubject: BehaviorSubject<User>;
-    public currentUser: Observable<User>;
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+  private serverAuthen: boolean;
 
-    constructor(private http: HttpClient) {
-        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
-        this.currentUser = this.currentUserSubject.asObservable();
-    }
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem("currentUser"))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
-    public get currentUserValue(): User {
-        return this.currentUserSubject.value;
-    }
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
 
-    login(username: string, password: string) {
-        return this.http.post<any>(`${environment.apiUrl}/api/user/login`, { 'username': username, 'password':password })
-            .pipe(map(user => {
-                // login successful if there's a jwt token in the response
-                if (user && user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    // localStorage.setItem('currentUser', JSON.stringify(user));
-                    // this.currentUserSubject.next(user);
-                }
+  login(username: string, password: string) {
+    return this.http
+      .post<any>(`${environment.apiUrl}/api/user/login`, {
+        username: username,
+        password: password,
+      })
+      .pipe(
+        map((user) => {
+          // login successful if there's a jwt token in the response
+          if (user && user.token) {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem("currentUser", JSON.stringify(user));
+            this.currentUserSubject.next(user);
+          }
 
-                return user;
-            }));
-    }
+          return user;
+        })
+      );
+  }
 
-    register(data: string) {
-        return this.http.post<any>(`${environment.apiUrl}/api/user/register`, { 'model': data });
-    }
+  register(data: string) {
+    return this.http.post<any>(`${environment.apiUrl}/api/user/register`, {
+      model: data,
+    });
+  }
 
-    recover(userNameOrEmail: string) {
-        return this.http.post<any>
-        (`${environment.apiUrl}/api/user/recover`, { 'UserOEmail': userNameOrEmail });
-    }
+  recover(userNameOrEmail: string) {
+    return this.http.post<any>(`${environment.apiUrl}/api/user/recover`, {
+      UserOEmail: userNameOrEmail,
+    });
+  }
 
-    confirm(email: string, token: string, pass: string) {
-        return this.http.post<any>
-        (`${environment.apiUrl}/api/user/confirm`, { 'email': email, 'pass': pass, 'token':token});
-    }
+  confirm(email: string, token: string, pass: string) {
+    return this.http.post<any>(`${environment.apiUrl}/api/user/confirm`, {
+      email: email,
+      pass: pass,
+      token: token,
+    });
+  }
 
-    validUserName(userName: string) {
-        let params = new HttpParams().set( 'username', userName );
-        return this.http.get<any>
-        (`${environment.apiUrl}/api/user/validusername`, { params: params });
-    }
+  validUserName(userName: string) {
+    let params = new HttpParams().set("username", userName);
+    return this.http.get<any>(`${environment.apiUrl}/api/user/validusername`, {
+      params: params,
+    });
+  }
 
-    isAuthenticated() {
-        var user = localStorage.getItem('currentUser');
+  isAuthenticated() {
+    return this.http.get<any>(`${environment.apiUrl}/api/user/ExistToken`);
+  }
 
-        if(user) {
-            return true;
-        }
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem("currentUser");
 
-        return false;
-    }
-
-    logout() {
-        // remove user from local storage to log user out
-        localStorage.removeItem('currentUser');
+    return this.http
+      .post<any>(`${environment.apiUrl}/api/user/logout`, null)
+      .subscribe((ret) => {
         this.currentUserSubject.next(null);
-    }
+      });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
-﻿import { Component, Injector, ViewEncapsulation } from '@angular/core';
-import { User } from '@app/_models';
-import { AuthenticationService, SiteLoader } from '@app/_services';
-import { map } from 'rxjs/operators';
-import { ContentSite, DEAFULT_IMAGE } from '@app/shared/models/contentsite.model';
-import { Events } from '@app/shared/Models/Events.model';
-import { ExternalProduct } from '@app/shared/Models/ExternalProduct.model';
+﻿import { Component, Injector, ViewEncapsulation } from "@angular/core";
+import { User } from "@app/_models";
+import { AuthenticationService, SiteLoader } from "@app/_services";
+import { map } from "rxjs/operators";
+import {
+  ContentSite,
+  DEAFULT_IMAGE,
+} from "@app/shared/models/contentsite.model";
+import { Events } from "@app/shared/Models/Events.model";
+import { ExternalProduct } from "@app/shared/Models/ExternalProduct.model";
 
 declare function recortarTituloPrincipal(text);
 declare function recortarSummary(text);
@@ -14,95 +17,148 @@ declare function recortarTituloProductoExterno(text);
 declare function recortarHeaderProductoExterno(text);
 declare function recortarDescriptionProductoExterno(text);
 
-@Component(
-    {
-        templateUrl: 'home.component.html' ,
-        styleUrls: ['home.component.css'],
-        encapsulation: ViewEncapsulation.None
-    })
+@Component({
+  templateUrl: "home.component.html",
+  styleUrls: ["home.component.css"],
+  encapsulation: ViewEncapsulation.None,
+})
 export class HomeComponent {
-    loading = false;
-    currentUser: User;
-    userFromApi: User;
-    contentSite: ContentSite;
-    events: Events[];
-    externalProduct: ExternalProduct[];
+  loading = false;
+  currentUser: User;
+  userFromApi: User;
+  contentSite: ContentSite;
+  events: Events[];
+  externalProduct: ExternalProduct[];
 
-    constructor(
-        private siteLoader: SiteLoader,
-        private authenticationService: AuthenticationService
-    ) {
-        this.currentUser = this.authenticationService.currentUserValue;
-    }
+  constructor(
+    private siteLoader: SiteLoader,
+    private authenticationService: AuthenticationService
+  ) {
+    this.currentUser = this.authenticationService.currentUserValue;
+  }
 
-    ngOnInit() {
+  ngOnInit() {
+    const sectionName = "NOTICIAS";
+    const cantMax = 4;
 
+    this.siteLoader.bannerSubject.next({
+      main: true,
+      section: false,
+      news: false,
+    });
+    this.siteLoader
+      .getNews(sectionName, cantMax)
+      .pipe(
+        map((ret) => ret as ContentSite),
+        map((ret) => {
+          if (ret.items != undefined) {
+            ret.items[0] != undefined && ret.items[0].title != undefined
+              ? (ret.items[0].title = recortarTituloPrincipal(
+                  ret.items[0].title
+                ))
+              : false;
+            ret.items[0] != undefined && ret.items[0].summary != undefined
+              ? (ret.items[0].summary = recortarSummary(ret.items[0].summary))
+              : false;
+            ret.items[1] != undefined && ret.items[1].title != undefined
+              ? (ret.items[1].title = recortarTituloSecundario(
+                  ret.items[1].title
+                ))
+              : false;
+            ret.items[2] != undefined && ret.items[2].title != undefined
+              ? (ret.items[2].title = recortarTituloSecundario(
+                  ret.items[2].title
+                ))
+              : false;
+            ret.items[3] != undefined && ret.items[3].title != undefined
+              ? (ret.items[3].title = recortarTituloSecundario(
+                  ret.items[3].title
+                ))
+              : false;
 
-        const sectionName = "NOTICIAS";
-        const cantMax = 4;
+            for (let index = 0; index < 4; index++) {
+              if (
+                !ret.items[index].image ||
+                ret.items[index].image == null ||
+                ret.items[index].image.imageUrl == ""
+              )
+                ret.items[index].image = { imageUrl: DEAFULT_IMAGE };
+            }
+          }
 
-        this.siteLoader.bannerSubject.next({main: true, section: false, news: false});
-        this.siteLoader.getNews(sectionName,cantMax)
-        .pipe(
-            map(ret => ret as ContentSite),
-            map(ret => {
-                if (ret.items != undefined){
-                    ret.items[0] != undefined && ret.items[0].title != undefined ? ret.items[0].title = recortarTituloPrincipal(ret.items[0].title) : false;
-                    ret.items[0] != undefined && ret.items[0].summary != undefined ? ret.items[0].summary = recortarSummary(ret.items[0].summary) : false;
-                    ret.items[1] != undefined && ret.items[1].title != undefined ? ret.items[1].title = recortarTituloSecundario(ret.items[1].title) : false;
-                    ret.items[2] != undefined && ret.items[2].title != undefined ? ret.items[2].title = recortarTituloSecundario(ret.items[2].title): false;
-                    ret.items[3] != undefined && ret.items[3].title != undefined ? ret.items[3].title = recortarTituloSecundario(ret.items[3].title): false;
+          return ret;
+        })
+      )
+      .subscribe((data) => {
+        this.contentSite = data;
+      });
 
-                    for (let index = 0; index < 4; index++) {
-                        if (!ret.items[index].image || ret.items[index].image == null || ret.items[index].image.imageUrl == '')
-                        ret.items[index].image = {imageUrl: DEAFULT_IMAGE};
-                    }    
-                    
-                    
-                }
+    this.siteLoader
+      .getEvents()
+      .pipe(map((ret) => ret as Events[]))
+      .subscribe((data) => {
+        this.events = data;
+      });
 
-                return ret;
-            })
-            )
-        .subscribe(data => {
-            this.contentSite = data;
-        });
+    this.siteLoader
+      .getExternalProducts()
+      .pipe(
+        map((ret) => ret as ExternalProduct[]),
+        map((ret) => {
+          if (ret != undefined) {
+            ret[0] != undefined && ret[0].title != undefined
+              ? (ret[0].title = recortarTituloProductoExterno(ret[0].title))
+              : false;
+            ret[1] != undefined && ret[1].title != undefined
+              ? (ret[1].title = recortarTituloProductoExterno(ret[1].title))
+              : false;
+            ret[2] != undefined && ret[2].title != undefined
+              ? (ret[2].title = recortarTituloProductoExterno(ret[2].title))
+              : false;
+            ret[3] != undefined && ret[3].title != undefined
+              ? (ret[3].title = recortarTituloProductoExterno(ret[3].title))
+              : false;
 
-        this.siteLoader.getEvents()
-        .pipe(map(ret => ret as Events[]))
-        .subscribe(data => {
-            this.events = data;
-        });
+            ret[0] != undefined && ret[0].header != undefined
+              ? (ret[0].header = recortarHeaderProductoExterno(ret[0].header))
+              : false;
+            ret[1] != undefined && ret[1].header != undefined
+              ? (ret[1].header = recortarHeaderProductoExterno(ret[1].header))
+              : false;
+            ret[2] != undefined && ret[2].header != undefined
+              ? (ret[2].header = recortarHeaderProductoExterno(ret[2].header))
+              : false;
+            ret[3] != undefined && ret[3].header != undefined
+              ? (ret[3].header = recortarHeaderProductoExterno(ret[3].header))
+              : false;
 
-        this.siteLoader.getExternalProducts()
-        .pipe(
-            map(ret => ret as ExternalProduct[]),
-            map(ret => {
-                if (ret != undefined) {
-                    ret[0] != undefined && ret[0].title != undefined ? ret[0].title = recortarTituloProductoExterno(ret[0].title) : false;
-                    ret[1] != undefined && ret[1].title != undefined ? ret[1].title = recortarTituloProductoExterno(ret[1].title) : false;
-                    ret[2] != undefined && ret[2].title != undefined ? ret[2].title = recortarTituloProductoExterno(ret[2].title) : false;
-                    ret[3] != undefined && ret[3].title != undefined ? ret[3].title = recortarTituloProductoExterno(ret[3].title) : false;
+            ret[0] != undefined && ret[0].description != undefined
+              ? (ret[0].description = recortarDescriptionProductoExterno(
+                  ret[0].description
+                ))
+              : false;
+            ret[1] != undefined && ret[1].description != undefined
+              ? (ret[1].description = recortarDescriptionProductoExterno(
+                  ret[1].description
+                ))
+              : false;
+            ret[2] != undefined && ret[2].description != undefined
+              ? (ret[2].description = recortarDescriptionProductoExterno(
+                  ret[2].description
+                ))
+              : false;
+            ret[3] != undefined && ret[3].description != undefined
+              ? (ret[3].description = recortarDescriptionProductoExterno(
+                  ret[3].description
+                ))
+              : false;
+          }
 
-                    ret[0] != undefined && ret[0].header != undefined ? ret[0].header = recortarHeaderProductoExterno(ret[0].header) : false;
-                    ret[1] != undefined && ret[1].header != undefined ? ret[1].header = recortarHeaderProductoExterno(ret[1].header) : false;
-                    ret[2] != undefined && ret[2].header != undefined ? ret[2].header = recortarHeaderProductoExterno(ret[2].header) : false;
-                    ret[3] != undefined && ret[3].header != undefined ? ret[3].header = recortarHeaderProductoExterno(ret[3].header) : false;
-
-                    ret[0] != undefined && ret[0].description != undefined ? ret[0].description = recortarDescriptionProductoExterno(ret[0].description) : false;
-                    ret[1] != undefined && ret[1].description != undefined ? ret[1].description = recortarDescriptionProductoExterno(ret[1].description) : false;
-                    ret[2] != undefined && ret[2].description != undefined ? ret[2].description = recortarDescriptionProductoExterno(ret[2].description) : false;
-                    ret[3] != undefined && ret[3].description != undefined ? ret[3].description = recortarDescriptionProductoExterno(ret[3].description) : false;
-                }
-            
-                return ret;
-            })
-        )
-        .subscribe(data => {
-            this.externalProduct = data;
-        });
-
-        
-    }
-
+          return ret;
+        })
+      )
+      .subscribe((data) => {
+        this.externalProduct = data;
+      });
+  }
 }
