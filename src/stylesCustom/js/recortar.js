e6c136587e6e9c019df48b9736e9fcb5c8c150df
Alberto Gonzalez <alberto.gonzalez@avaco.com.ar>
2020-04-29 17:56:41 -0300

    ajustes del recorte

--- src/stylesCustom/js/recortar.js
+++ src/stylesCustom/js/recortar.js
@@ -1,5 +1,3 @@
-
-
 $.fn.textWidth = function(text, font) {
     if (!$.fn.textWidth.fakeEl) {
         $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
@@ -15,9 +13,25 @@ $.fn.primerAjuste = function(text) {
     return text;
 }
 
-$.fn.recortar = function(text, font, lineWidth) {
+/*$.fn.recortar = function(text, font, lineWidth) {
     if (text != undefined && text != null) {
-        var retext = $.fn.primerAjuste(text);
+        
+        var modified = text.replace('&nbsp;',' ');
+        
+        modified = modified.replace(new RegExp('&aacute;', "g"),'á');
+        modified = modified.replace(new RegExp('&eacute;', "g"),'é');
+        modified = modified.replace(new RegExp('&iacute;', "g"),'í');
+        modified = modified.replace(new RegExp('&oacute;', "g"),'ó');
+        modified = modified.replace(new RegExp('&uacute;', "g"),'ú');
+        
+        modified = modified.replace(new RegExp('&Aacute;', "g"),'Á');
+        modified = modified.replace(new RegExp('&Eacute;', "g"),'É');
+        modified = modified.replace(new RegExp('&Iacute;', "g"),'Í');
+        modified = modified.replace(new RegExp('&Oacute;', "g"),'Ó');
+        modified = modified.replace(new RegExp('&Uacute;', "g"),'Ú');
+        
+        var retext = $.fn.primerAjuste(modified);
+        
         var size = $.fn.textWidth(retext, font);
         var recortar = size > lineWidth || text.length > retext.length;
         while (size > lineWidth) {
@@ -29,10 +43,23 @@ $.fn.recortar = function(text, font, lineWidth) {
             retext = text.toString().substr(0,retext.length-5);
             retext = retext + '...'
         }
-        return retext;
+
+        modified = modified.replace(new RegExp('á', "g"),'&aacute;');
+        modified = modified.replace(new RegExp('é', "g"),'&eacute;');
+        modified = modified.replace(new RegExp('í', "g"),'&iacute;');
+        modified = modified.replace(new RegExp('ó', "g"),'&oacute;');
+        modified = modified.replace(new RegExp('ú', "g"),'&uacute;');
+
+        modified = modified.replace(new RegExp('Á', "g"),'&Aacute;');
+        modified = modified.replace(new RegExp('É', "g"),'&Eacute;');
+        modified = modified.replace(new RegExp('Í', "g"),'&Iacute;');
+        modified = modified.replace(new RegExp('Ó', "g"),'&Oacute;');
+        modified = modified.replace(new RegExp('Ú', "g"),'&Uacute;');
+        
+        return modified;
     }
     return "";
-};
+};*/
 
 $.fn.recortar2 = function(text, font, lineWidth) {
     if (text != undefined && text != null) {
@@ -70,57 +97,92 @@ $.fn.recortar2 = function(text, font, lineWidth) {
 };
 
 function recortarTituloPrincipal(text) {
-    return $.fn.recortar(text, '23pt Roboto', 900);
+    return $.fn.recortar2(text, '23pt Roboto', 900);
 }
 
 function recortarTituloSecundario(text) {
-    return $.fn.recortar(text, '20px Roboto', 950);
+    return $.fn.recortar2(text, '20px Roboto', 950);
 }
 
 function recortarSummary(text) {
-    return $.fn.recortar(text, '12pt Roboto', 1800);
+    return reemplazoCaracteres(text, '12pt Roboto', 1700);
 }
 
 function recortarTituloProductoExterno(text) {
-    return $.fn.recortar(text, '16px Roboto', 230);
+    return $.fn.recortar2(text, '16px Roboto', 230);
 }
 
 function recortarHeaderProductoExterno(text) {
-    return $.fn.recortar(text, '20px Roboto', 420);
+    return $.fn.recortar2(text, '20px Roboto', 380);
 }
 
 function recortarDescriptionProductoExterno(text) {
-    return $.fn.recortar(text, '16px Roboto', 1000);
+    return $.fn.recortar2(text, '16px Roboto', 1000);
 }
 
 function recortarTituloListado(text)  {
-    return $.fn.recortar(text, '26px sans-serif', 940);
+    return reemplazoCaracteres(text, '26px sans-serif', 800);
 }
 
 function recortarSummaryListado(text) {
-    return $.fn.recortar(text, '12px sans-serif', 1710);
+    return reemplazoCaracteres(text, '12px sans-serif', 1400);
 }
 
 function recortarTituloListadoTemplateOne(text)  {
-    return $.fn.recortar(text, '26px sans-serif', 800);
+    return reemplazoCaracteres(text, '26px sans-serif', 630);
 }
 
 function recortarSummaryListadoTemplateOne(text) {
-    return $.fn.recortar(text, '14px sans-serif', 1360);
+    return reemplazoCaracteres(text, '14px sans-serif', 1260);
 }
 
 function recortarTituloBeneficio(text)  {
-    return $.fn.recortar(text, '26px sans-serif', 460);
+    return reemplazoCaracteres(text, '26px sans-serif', 420);
 }
 
 function recortarSummaryBeneficio(text) {
-    return $.fn.recortar(text, '14px sans-serif', 1140);
+    return reemplazoCaracteres(text, '14px sans-serif', 1000);
 }
 
 function recortarTituloListadoTemplateFour(text)  {
-    return $.fn.recortar2(text, '26px sans-serif', 1400);
+    return reemplazoCaracteres(text, '26px sans-serif', 1300);
 }
 
 function recortarSummaryListadoTemplateFour(text) {
-    return $.fn.recortar2(text, '14px sans-serif', 2250);
+        return reemplazoCaracteres(text, '14px sans-serif', 2050);
+}
+
+function reemplazoCaracteres(text, tipografia, size) {
+        if (text){
+        var modified = text.replace('&nbsp;',' ');
+        
+        modified = modified.replace(new RegExp('&aacute;', "g"),'á');
+        modified = modified.replace(new RegExp('&eacute;', "g"),'é');
+        modified = modified.replace(new RegExp('&iacute;', "g"),'í');
+        modified = modified.replace(new RegExp('&oacute;', "g"),'ó');
+        modified = modified.replace(new RegExp('&uacute;', "g"),'ú');
+        
+        modified = modified.replace(new RegExp('&Aacute;', "g"),'Á');
+        modified = modified.replace(new RegExp('&Eacute;', "g"),'É');
+        modified = modified.replace(new RegExp('&Iacute;', "g"),'Í');
+        modified = modified.replace(new RegExp('&Oacute;', "g"),'Ó');
+        modified = modified.replace(new RegExp('&Uacute;', "g"),'Ú');
+    
+        modified = $.fn.recortar2(modified, tipografia, size);
+
+        modified = modified.replace(new RegExp('á', "g"),'&aacute;');
+        modified = modified.replace(new RegExp('é', "g"),'&eacute;');
+        modified = modified.replace(new RegExp('í', "g"),'&iacute;');
+        modified = modified.replace(new RegExp('ó', "g"),'&oacute;');
+        modified = modified.replace(new RegExp('ú', "g"),'&uacute;');
+
+        modified = modified.replace(new RegExp('Á', "g"),'&Aacute;');
+        modified = modified.replace(new RegExp('É', "g"),'&Eacute;');
+        modified = modified.replace(new RegExp('Í', "g"),'&Iacute;');
+        modified = modified.replace(new RegExp('Ó', "g"),'&Oacute;');
+        modified = modified.replace(new RegExp('Ú', "g"),'&Uacute;');
+        
+        return modified;
+    }
+    return "";
 }
