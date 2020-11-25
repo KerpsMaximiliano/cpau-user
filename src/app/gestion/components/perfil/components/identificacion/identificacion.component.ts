73f8a6fa6dd0627988c8a2a50390f4cfc928f0ca
alexisjrossi <alexisjrossi@gmail.com>
2020-11-25 11:01:06 -0300

    shown validations errors

--- src/app/gestion/components/perfil/components/identificacion/identificacion.component.ts
+++ src/app/gestion/components/perfil/components/identificacion/identificacion.component.ts
@@ -1,40 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SelectItem } from '@app/gestion/shared/Models/SelectItem.model';
+import { Observable } from 'rxjs';
 import { IdentificacionService } from './service/identificacion.service';
 
 @Component({
   selector: 'app-identificacion',
   templateUrl: './identificacion.component.html',
-  styleUrls: ['./identificacion.component.css']
+  styleUrls: ['./identificacion.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IdentificacionComponent implements OnInit {
 
 
   public identificacionForm: FormGroup;
 
-  public countries$ = this.identificacionService.countries$;
+  public countries$: Observable<SelectItem[]>;
 
-  public docTypes$ = this.identificacionService.docTypes$;
+  public docTypes$: Observable<SelectItem[]>;
 
   constructor(
     private formBuilder: FormBuilder,
     private identificacionService: IdentificacionService) {
 
     this.identificacionForm = this.formBuilder.group({
-      nombre: ['', [Validators.required, Validators.maxLength(200), Validators.minLength(2)]],
-      apellido: ['', [Validators.required, Validators.maxLength(200), Validators.minLength(2)]],
-      sexo: ['', [Validators.required]],
+      nombre: ['', {
+        validators: [Validators.required, Validators.maxLength(200), Validators.minLength(2)],
+        updateOn: 'blur'
+      }],
+      apellido: ['', {
+        validators: [Validators.required, Validators.maxLength(200), Validators.minLength(2)],
+        updateOn: 'blur'
+      }],
+      sexo: ['', {
+        validators: [Validators.required],
+        updateOn: 'blur'
+      }],
       tipoDoc: [],
-      nroDoc: ['', [Validators.required, Validators.max(99999999), Validators.min(1000000)]],
+      nroDoc: ['', {
+        validators: [Validators.required, Validators.max(99999999), Validators.min(1000000)],
+        updateOn: 'blur'
+
+      }],
       fechaNac: [],
       paisNac: []
     });
+
+    this.countries$ = this.identificacionService.countries$;
+    this.docTypes$ = this.identificacionService.docTypes$;
   }
 
   ngOnInit() {
   }
 
   onClick() {
-    console.log(this.identificacionForm.value);
+    this.identificacionForm.markAllAsTouched();
+    console.log(this.identificacionForm);
   }
 }
