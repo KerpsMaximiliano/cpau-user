e8ac8b98aad506010e6aa612ab81ded749aaf2fc
Omar Villalba <ovillalba@szicom.com.ar>
2021-02-17 21:46:18 -0300

    Fix estilos según app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
-﻿import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+﻿import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 import { AuthenticationService } from './_services';
 import { User, Role } from './_models';
+import { DomSanitizer } from '@angular/platform-browser';
+import { filter } from 'rxjs/operators';
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
     currentUser: User;
+    dynamicCSSUrl: string;
+    dynamicCSSUrlCpau: string;
 
     constructor(
         private router: Router,
-        private authenticationService: AuthenticationService
+        private authenticationService: AuthenticationService,
+        private sanitizer: DomSanitizer
     ) {
         this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+
+        router.events.pipe(
+            filter(event => event instanceof NavigationEnd)  
+          ).subscribe((event: NavigationEnd) => {
+            if (event.url == '/') {
+                this.dynamicCSSUrl = '../stylesCustom/styles/bootstrap/bootstrap.css';
+                this.dynamicCSSUrlCpau = '../stylesCustom/styles/cpau.css';
+            } else if (event.url == '/gestion/home/perfil/identificacion') {
+                this.dynamicCSSUrl = '../stylesCustom/styles/bootstrap/bootstrap.3.3.4.css';
+            }
+          });
     }
 
     get isAdmin() {
