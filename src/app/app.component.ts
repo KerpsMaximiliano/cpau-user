8e64f77a63b43adc8f742dd73052848053575406
Omar Villalba <ovillalba@szicom.com.ar>
2021-02-21 11:24:21 -0300

    Estilo por Institucional y perfil privado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,19 +18,6 @@ export class AppComponent {
         private sanitizer: DomSanitizer
     ) {
         this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-
-        router.events.pipe(
-            filter(event => event instanceof NavigationEnd)  
-          ).subscribe((event: NavigationEnd) => {
-            console.log('Log:');
-            console.log(event.url);
-            if (event.url == '/') {
-                this.dynamicCSSUrl = '../stylesCustom/styles/bootstrap/bootstrap.css';
-                this.dynamicCSSUrlCpau = '../stylesCustom/styles/cpau.css';
-            } else if (event.url == '/gestion/home/perfil/identificacion') {
-                this.dynamicCSSUrl = '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css';
-            }
-          });
     }
 
     get isAdmin() {
