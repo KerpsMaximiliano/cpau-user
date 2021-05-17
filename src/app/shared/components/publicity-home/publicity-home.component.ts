8cf595610081b9c2ab1a7baefdde36b62d788db9
Omar Villalba <ovillalba@szicom.com.ar>
2021-05-16 23:51:34 -0300

    Carousel

--- src/app/shared/components/publicity-home/publicity-home.component.ts
+++ src/app/shared/components/publicity-home/publicity-home.component.ts
@@ -1,9 +1,6 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SiteLoader } from '@app/_services';
 declare var $: any;
-// import * as jQuery from 'jquery';
-// window['$'] = jQuery;
-// import * as $ from 'jquery'; window["$"] = $; window["jQuery"] = $;
 
 @Component({
   selector: 'app-publicity-home',
@@ -12,27 +9,56 @@ declare var $: any;
 })
 export class PublicityHomeComponent implements OnInit {
   banners: any = [];
+  load: boolean;
 
-  constructor(private siteLoad: SiteLoader,
-              private el: ElementRef) {
-                $(this.el.nativeElement).on('slide.bs.carousel', (e: any) =>  {
-                  const $e = $(e.relatedTarget);
-                  const idx = $e.index();
-                  const itemsPerSlide = 4;
-                  const totalItems = $('.carousel-item').length;
+  constructor(private siteLoad: SiteLoader) {
+  }
 
-                  if (totalItems > itemsPerSlide) {
-                      const it = itemsPerSlide - (totalItems - idx);
-                      for (let i = 0; i < it; i++) {
-                          // append slides to end
-                          if (e.direction === 'left') {
-                              $('.carousel-item').eq(i).appendTo('.carousel-inner');
-                          } else {
-                              $('.carousel-item').eq(0).appendTo('.carousel-inner');
-                          }
-                      }
-                  }
-               });
+  /* Helpers */
+  messagesRendered(isLast: boolean) {
+    if (isLast && !this.load) {
+      this.carrousel();
+    }
+  }
+
+  carrousel() {
+    this.load = true;
+    $('#myCarousel').carousel( {
+      interval: 6000
+    });
+
+    $('.carousel .item').each(function(){
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+    });
+
+    $('#myCarousel').bind('slide.bs.carousel', function(e) {
+
+      var $e = $(e.currentTarget);
+      var totalItems = $('.carousel .item').length - 1;
+      var $indicators = $e.find('.carousel-inner');
+
+      var $item = $(e.relatedTarget);
+      
+      var active = $e.find('.active');
+      var itemIndex = active.index();
+      
+      active.removeClass('active');
+      $indicators.children().eq(itemIndex < totalItems ? itemIndex + 1: 0).addClass('active');
+
+    });
   }
 
   ngOnInit() {
@@ -41,7 +67,7 @@ export class PublicityHomeComponent implements OnInit {
        .subscribe(ret => {
         this.banners = ret
           .filter(x => {
-              if(data.main && x.isMainPage)
+              if(data.main && x.isMainPage) 
                   return x;
 
               if(data.section && x.isSection)
@@ -49,8 +75,7 @@ export class PublicityHomeComponent implements OnInit {
 
               if(data.news && x.isNewsletter)
                   return x;
-          }
-        );
+          });
       }
       )
       ).unsubscribe();
