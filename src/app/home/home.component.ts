fac5d977ad6f8e0511baddd18be331e4929e47f4
Alberto Gonzalez <alosgonzalez@gmail.com>
2021-06-01 18:33:06 -0300

    link target en producto externo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -196,4 +196,28 @@ export class HomeComponent {
       }
     });
   }
+
+  selectTarget(index){
+    let target = '';
+    switch (index) {
+      case 0:
+        target = '_self';
+        break;
+      case 1:
+        target = '_blank';        
+        break;
+      case 2:
+        target = '_parent';        
+        break;
+      case 3:
+        target = '_top';    
+        break;
+      case 4:
+        target = '_search';    
+        break;
+      default:
+        break;
+    }
+    return target;
+  }
 }
