2a05444baba27a24cad782856f7b6b75d1e33268
LAPTOP-FREPM0D8\Omar Villalba <omar.m.villalba@gmail.com>
2021-10-01 14:57:28 -0300

    Cambio de generacion de FORM

--- src/app/gestion/components/matricula/components/derecho-anual/pagar-boleta/pagar-boleta.component.ts
+++ src/app/gestion/components/matricula/components/derecho-anual/pagar-boleta/pagar-boleta.component.ts
@@ -16,6 +16,7 @@ export class PagarBoletaComponent implements OnInit {
   formValidarPago: ValidarPago;
   debito: boolean = false;
   @ViewChild('form', { static: false }) form: ElementRef;
+  @ViewChild('emailCliente', { static: false }) email : ElementRef;
   @Output() cerrar = new EventEmitter();
   constructor(private derechoAnualService: DerechoAnualService) { }
 
@@ -23,17 +24,61 @@ export class PagarBoletaComponent implements OnInit {
   }
 
   select(medioDePago: string) {
-    this.derechoAnualService.pagarBoleta(medioDePago, this.cuotas).subscribe(data => {
-      this.formValidarPago = {
-        cuotas: data.cuotas,
-        emailCliente: data.emailCliente,
-        nroComercio: data.nroComercio,
-        nroOperacion: data.nroOperacion,
-        medioDePago: data.medioDePago,
-        monto: `${data.monto}00`
-      }
-      this.form.nativeElement.submit();
+    this.derechoAnualService.pagarBoleta(medioDePago, this.cuotas)
+    .subscribe(data => {
+      this.buildForm(data);
     })
+  }
+  buildForm(data: ValidarPago) {
+    
+    var my_form=document.createElement('FORM') as any;
+    my_form.method='POST';
+    my_form.action='https://sps.decidir.com/sps-ar/Validar';
+
+    var my_tb=document.createElement('INPUT') as any;
+    my_tb.type='HIDDEN';
+    my_tb.name='CUOTAS';
+    my_tb.value=data.cuotas;
+    my_form.appendChild(my_tb);
+
+    var my_tb=document.createElement('INPUT') as any;
+    my_tb.type='HIDDEN';
+    my_tb.name='NROCOMERCIO';
+    my_tb.value=data.nroComercio;
+    my_form.appendChild(my_tb);
+
+    var my_tb=document.createElement('INPUT') as any;
+    my_tb.type='HIDDEN';
+    my_tb.name='NROOPERACION';
+    my_tb.value=data.nroOperacion;
+    my_form.appendChild(my_tb);
+
+    var my_tb=document.createElement('INPUT') as any;
+    my_tb.type='HIDDEN';
+    my_tb.name='MEDIODEPAGO';
+    my_tb.value=data.medioDePago;
+    my_form.appendChild(my_tb);
+
+    var my_tb=document.createElement('INPUT') as any;
+    my_tb.type='HIDDEN';
+    my_tb.name='MONTO';
+    my_tb.value=`${data.monto}00`;
+    my_form.appendChild(my_tb);
+
+    var my_tb=document.createElement('INPUT') as any;
+    my_tb.type='HIDDEN';
+    my_tb.name='EMAILCLIENTE';
+    my_tb.value=data.emailCliente;
+    my_form.appendChild(my_tb);
+    
+    var my_tb=document.createElement('INPUT') as any;
+    my_tb.type='HIDDEN';
+    my_tb.name='URLDINAMICA';
+    my_tb.value='https://www.cpau.org/api/consejo/resultadooperacion';
+    my_form.appendChild(my_tb);
+
+    document.body.appendChild(my_form);
+    my_form.submit();
   }
 
 }
