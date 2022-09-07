ccc984f4690bc8bd075e66cfc4492f8f368df8cb
Avaco Sistemas <avacosistemas@gmail.com>
2022-09-07 12:53:05 -0300

    nuevo enviroment migracion

---
+++ src/environments/environment.migracion.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://cpauorgapi-migracion.azurewebsites.net',
+    oldSiteUrl: '',
+    storeUrl: 'https://test.cpau.org/store?length=4',
+	siteUrl: 'https://test.cpau.org/',
+	adminUrl: 'https://test-admin.cpau.org',
+	recaptcha: '6LdrlTAhAAAAAOu8PHdsj1IwMKybDPLbqGj_RfeC'
+};
