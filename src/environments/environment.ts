3dd20a2caab9666a283e6e214e102cb9cba64d66
Avaco Sistemas <avacosistemas@gmail.com>
2022-09-19 09:50:14 -0300

    ajustes logo formularios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,9 @@ export const environment = {
     production: true,
     storeUrl: 'http://localhost:50501/store?length=4',
 	adminUrl: '',
-    // recaptcha: '6LdrlTAhAAAAAOu8PHdsj1IwMKybDPLbqGj_RfeC', //localhost
-    recaptcha: '6LeoucchAAAAAJo8rKp1Qf0vsV6AWxSw_cI-jELs', // migracion
+    recaptcha: '6LdrlTAhAAAAAOu8PHdsj1IwMKybDPLbqGj_RfeC', //localhost
+    //recaptcha: '6LeoucchAAAAAJo8rKp1Qf0vsV6AWxSw_cI-jELs', // migracion
+    
     apiUrl: 'https://cpauorgapi-migracion.azurewebsites.net'
 };
 
