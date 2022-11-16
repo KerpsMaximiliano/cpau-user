a151359b7a562fffd3ce83098e1bd6b368f4bf51
Avaco Sistemas <avacosistemas@gmail.com>
2022-11-16 17:16:02 -0300

    Ajustes pantalla cambio contraseña por recupero

--- src/app/login/recover/confirm/confirm.component.ts
+++ src/app/login/recover/confirm/confirm.component.ts
@@ -15,6 +15,12 @@ export class ConfirmComponent implements OnInit {
   loading = false;
   token : string;
   email : string;
+  pass1requerida: boolean = false;
+  pass1invalida: boolean = false;
+  pass2requerida: boolean = false;
+  pass2invalida: boolean = false;
+  pass2nocoincide: boolean = false;
+  
 
   constructor(  private activatedRoute: ActivatedRoute, private router: Router, private formBuilder: FormBuilder, private authenticationService: AuthenticationService) { }
 
@@ -27,7 +33,7 @@ export class ConfirmComponent implements OnInit {
     });
 
     this.formsignin = this.formBuilder.group({
-      pass1: ['', [Validators.required,Validators.minLength(8),Validators.maxLength(50),Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$+%^\-_&\[\]*]')]],
+      pass1: ['', [Validators.required,Validators.minLength(8),Validators.maxLength(50),Validators. pattern('^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\\D*\\d)[A-Za-z\\d!@$+%#_*&]{8,}$')]],
       pass2: ['', Validators.required]
   });
   }
@@ -35,42 +41,56 @@ export class ConfirmComponent implements OnInit {
  // convenience getter for easy access to form fields
  get f() { return this.formsignin.controls; }
 
+  
+
   onSubmit() {
+
+    this.pass1requerida = false;
+    this.pass1invalida = false;
+    this.pass2requerida = false;
+    this.pass2invalida = false;
+    this.pass2nocoincide = false;
+
     // stop here if form is invalid
-    if (this.formsignin.invalid) {
-        this.formsignin.markAllAsTouched();
-        return;
+    this.pass1requerida = this.formsignin.controls.pass1.value.trim().length === 0;
+    if (!this.pass1requerida) {
+      if (this.formsignin.controls.pass1.errors && this.formsignin.controls.pass1.errors.pattern) {
+        this.pass1invalida = true;
+      }
     }
+    this.pass2requerida = this.formsignin.controls.pass2.value.trim().length === 0;
+
+    if (this.pass1requerida || this.pass1invalida || this.pass2requerida) return;
 
     if(this.f.pass1.value != this.f.pass2.value) {
-      document.getElementById('btnDatosIncorrectos').click();
+      this.pass2nocoincide = true;
       return;
     }
 
-    this.loading = true;
+    // this.loading = true;
 
-    this.authenticationService.confirm(this.email,this.token, this.f.pass1.value)
-        .pipe(first())
-        .subscribe(
-            data => {
-               if(data) {
-                   if(!data.success) {
-                      document.getElementById('btnDatosIncorrectos').click();
+    // this.authenticationService.confirm(this.email,this.token, this.f.pass1.value)
+    //     .pipe(first())
+    //     .subscribe(
+    //         data => {
+    //            if(data) {
+    //                if(!data.success) {
+    //                   document.getElementById('btnDatosIncorrectos').click();
 
-                    if(data.message)
-                      console.log(data);
+    //                 if(data.message)
+    //                   console.log(data);
 
-                   } else {
-                    this.ButtonText = "PROCESO COMPLETADO CON EXITO";
+    //                } else {
+    //                 this.ButtonText = "PROCESO COMPLETADO CON EXITO";
 
-                    setTimeout(() => {
-                      this.router.navigate(['/login']);
-                    }, 3000);
-                   }
-               }
-            },
-            error => {
-                this.loading = false;
-            });
+    //                 setTimeout(() => {
+    //                   this.router.navigate(['/login']);
+    //                 }, 3000);
+    //                }
+    //            }
+    //         },
+    //         error => {
+    //             this.loading = false;
+    //         });
 }
 }
