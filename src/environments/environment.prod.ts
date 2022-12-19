93f933ffa960e99727ff317c229a49432437f3e9
Avaco Sistemas <avacosistemas@gmail.com>
2022-12-19 12:15:21 -0300

    recaptchas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'https://cpauorgapi.azurewebsites.net',
     adminUrl: 'https://admin.cpau.org',
+    recaptcha: '6Lcx9o8jAAAAACt1r3EKWVryISo0kCSmSoRF1glQ'
 };
